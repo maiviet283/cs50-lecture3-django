f69c0a6e6b192db4c1d84ddad300182796474476
tfboys2016 <duvimuahe123@gmail.com>
2024-10-09 14:44:25 +0700

    first commit

---
+++ hello/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return render(request,"hello/index.html")
+
+def brian(request):
+    return HttpResponse("Hello, Brian!")
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+# def greet(request, name):
+#     return HttpResponse(f"Hello, {name.capitalize()}")
+
+def greet(request, name):
+    return render(request,"hello/greet.html",{
+        "name": name.capitalize()
+    })
